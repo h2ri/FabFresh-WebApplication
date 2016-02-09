1131885760017e33a46dede5d52533380eed7c36
pawan1794 <pawan1794@gmail.com>
2016-02-09 12:18:03 +0530

    listing the orders

--- app/scripts/controllers/ordersCTRL.js
+++ app/scripts/controllers/ordersCTRL.js
@@ -10,7 +10,7 @@ routerApp
     $http({
       method  : 'GET',
       url     : URL+'/orders/',
-      headers : {'Authorization': 'Bearer '+$rootScope.access_token} 
+      headers : {'Authorization': 'Bearer '+"hari"} 
      })
       .success(function(data) {
         if (data.errors) {
@@ -23,27 +23,38 @@ routerApp
           type["1"] = "Iron";
           type["2"] = "Wash and Iron";
 
-          var type = {};
-          type["0"] = "cancelled";
-          type["1"] = "created";
-          type["2"] = "pickup";
-          type["3"] = "receivedAtCenter";
-          type["4"] = "precheck";
-          type["5"] = "tagging";
-          type["6"] = "wash";
-          type["7"] = "dry";
-          type["8"] = "iron";
-          type["9"] = "package";
-          type["10"] = "shipped";
-          type["11"] = "drop";
-          type["12"] = "completed";
+          var type1 = {};
+          type1["0"] = "cancelled";
+          type1["1"] = "created";
+          type1["2"] = "pickup";
+          type1["3"] = "receivedAtCenter";
+          type1["4"] = "precheck";
+          type1["5"] = "tagging";
+          type1["6"] = "wash";
+          type1["7"] = "dry";
+          type1["8"] = "iron";
+          type1["9"] = "package";
+          type1["10"] = "shipped";
+          type1["11"] = "drop";
+          type1["12"] = "completed";
 
           $scope.data=data;
           for(var i=0;i<data.length;i++){
+            var str = data[i].created_at_time.toString();
               $scope.data[i].order_type=type[data[i].order_type];
-              $scope.data[i].status=type[data[i].status];
+              $scope.data[i].status=type1[data[i].status];
+              $scope.data[i].created_at_time= str.substring(11,18)+", "+str.substring(0,10);
               if(data[i].amount==null)
                   $scope.data[i].amount=0;
+              if(data[i].weight==null)
+                  $scope.data[i].weight=0;
+              if(data[i].quantity==null)
+                  $scope.data[i].quantity=0;
+              if(data[i].coupon==null)
+                  $scope.data[i].coupon="No Coupaon";
+              if(data[i].afterDiscount==null)
+                  $scope.data[i].afterDiscount="No Discount";
+
           }
 
         }
